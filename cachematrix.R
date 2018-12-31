6704637a2fecc86e1798d06d1e2e13f89f76cf32
juanky201271 <juanky201271@gmail.com>
2018-12-30 20:18:32 -0700

    Download the two functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We store in cache a matrix and the inverse.
+## The first time only store the matrix and its inverse, and the 
+## next time just view the data store in the environment
 
-## Write a short comment describing this function
+## Store the matrix, and create the four functions for manage 
+## the data stored (the matrix and the inverse)
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+  inv <- NULL
+  setmat <- function(mat2) {
+    mat <<- mat2
+    inv <<- NULL
+  }
+  getmat <- function() mat
+  setinv <- function(inv2) inv <<- inv2
+  getinv <- function() inv
+  list(setmat = setmat, getmat = getmat,
+       setinv = setinv, getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## Do the Matrix's inverse, and manage the Environment's cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'mat'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$getmat()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
